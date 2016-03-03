0f34a00fae79bec7ef7494465bee73436d6d036e
NetWilliam <NWm.LWB@gmail.com>
2016-03-03 11:23:20 +0800

    modify pseudo code almost done

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python
 #! coding: utf-8
 
-import Queue
+#import Queue
 import threading
 import gevent
 from gevent import queue
 import configure
 
-interval = 0.1
+interval = 0.1 # 0.1 second
 
 
 class FileWatcher():
 
-    def __init__(self, monitors, consumer_queues):
+    def __init__(self, monitors, monitor_filter_queues):
         self._monitors = monitors
-        self._consumer_queues = consumer_queues
+        self._monitor_filter_queues = monitor_filter_queues
 
     def start_watch_loop(self):
         while True:
             print "passive loop forever"
 
     def on_event_file_modify(self, event):
-        dispatcher(event.message, self._consumer_queues)
+        dispatch_file_mod(event.message, self._monitor_filter_queues)
 
 
-def dispatcher(task, consumer_queues):
-    for i in consumer_queues:
+def dispatch_file_mod(task, monitor_filter_queues):
+    for i in monitor_filter_queues:
         i.put(task)
 
 
 class Worker():
 
-    def __init__(self, monitor, consumer_queue):
+    def __init__(self, monitor, monitor_filter_queue):
         self._monitor = monitor
-        self._consumer_queue = consumer_queue
+        self._consumer_queue = monitor_filter_queue
 
     def work(self):
         while True:
@@ -55,13 +55,13 @@ def start_monitor(monitor_queue):
     monitors = configure.get_monitors()
     monitor_cnt = len(monitors)
 
-    consumer_queues = [queue.Queue() for i in xrange(monitor_cnt)]
-    workers = [Worker(monitors[i], consumer_queues[i]) for i in xrange(monitor_cnt)]
+    monitor_filter_queues = [queue.Queue() for i in xrange(monitor_cnt)]
+    workers = [Worker(monitors[i], monitor_filter_queues[i]) for i in xrange(monitor_cnt)]
 
     timer = threading.Timer(interval, on_point_one_second_timer, [monitors, monitor_queue])
     timer.start()
 
-    file_watcher = FileWatcher(monitors, consumer_queues)
+    file_watcher = FileWatcher(monitors, monitor_filter_queues)
     jobs = [
         gevent.spawn(file_watcher.start_watch_loop),
     ] + [
@@ -73,22 +73,52 @@ def start_monitor(monitor_queue):
 
 class WarningFilter():
 
-    def __init__(self, name, formula, trigger_cycle, continuous_cycle, alert):
-        self._name = name
-        self._formula = formula
-        self._trigger_cycle = trigger_cycle
-        self._continuous_cycle = continuous_cycle
-        self._alert = alert
-        self._queue = Queue.Queue(maxsize=self._continuous_cycle)
+    def __init__(self, warning, warning_filter_queues):
+        self._name = warning.name
+        self._formula = warning.formula
+        self._trigger_cycle = warning.trigger_cycle
+        self._continuous_cycle = warning.continuous_cycle
+        self._alert = warning.alert
+        self._monitor_type = warning.monitor_type
+        self._queue = queue.Queue(maxsize=self._continuous_cycle)
+        warning_filter_queues[self._monitor_type].append(self._queue)
 
-    def on_message_received(self, data):
-        pass
+    def register_message(self):
+        while True:
+            self._next_message = self._queue.get()
+            self.filter_and_warning()
+
+    def filter_and_warning(self):
+        pass # do warning checking and alerting work
+
+
+def on_monitor_message_received(monitor_queue, warning_filter_queues):
+    while True:
+        new_monitor_messages = monitor_queue.get()
+        dispatch_warning(new_monitor_messages, warning_filter_queues)
+
+
+def dispatch_warning(monitor_messages, warning_filter_queues):
+    for i in xrange(len(warning_filter_queues)):
+        monitor_message = monitor_messages[i]
+        for message_queue in warning_filter_queues:
+            message_queue.put[monitor_message]
 
 
 def start_warning_filer(monitor_queue):
     warnings = configure.get_warnings()
     warning_cnt = len(warnings)
     print warning_cnt
+    warning_filter_queues = [[] for i in xrange(configure.monitor_type_kinds)] # noqa
+    if len(warning_filter_queues) != len(monitor_queue):
+        pass # error handle
+    filters = [WarningFilter(warning) for warning in warnings]
+    jobs = [
+        gevent.spawn(on_monitor_message_received, monitor_queue, warning_filter_queues)
+    ] + [
+        gevent.spawn(filter.register_message) for filter in filters
+    ]
+    return jobs
 
 
 if __name__ == "__main__":
